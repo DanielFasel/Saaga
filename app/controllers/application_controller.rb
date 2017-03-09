39841eb8efb8113630d73d21fd239b71f8f85a87
DanielFasel <daniel.fasel@protonmail.com>
2017-03-09 12:47:42 +0200

    working language and translation
    Without actual translations and with broken menu to select languages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,12 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :set_locale
 
   # To allow other values
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  before_filter :set_user_region
 
   # redirects after Sign-in.
   def after_sign_in_path_for(user)
@@ -18,15 +20,50 @@ class ApplicationController < ActionController::Base
   end
 
 
+
+
+
+
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+
+
+
+
+
+
   protected
 
     # Checks if receiving data and type from form then makes sure no other type was used.
     def configure_permitted_parameters
-      if params.key?(:user) && params[:user].key?(:type)
-      raise "YOU SHALL NOT PASS!" unless params[:user][:type].in?(['Student', 'Teacher'])
+
+      if params.key?(:user) && params[:user].key?(:type) && params[:user].key?(:region)
+
+        raise "YOU SHALL NOT PASS!" unless params[:user][:type].in?(['Student', 'Teacher'])
+        raise "invalid language" unless params[:user][:region].in?(['en','fi'])
+
       end
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:type, :email, :password, :password_confirmation) }
+
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:type, :email, :password, :password_confirmation, :region) }
     end
 
 
+
+
+    private
+
+
+      def set_user_region
+        I18n.locale = current_user.region if user_signed_in?
+      end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,22 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  # root page
-  root 'homepages#index'
+  scope "(:locale)", locale: /en|fi/ do
+
+    # root page
+    root 'homepages#index'
 
 
-  # routes to the mainpages
-  get '/homeworks', to: 'homeworks#index'
-  get '/classes', to: 'classes#index'
-  get '/exercises', to: 'exercises#index'
-  get '/scourses', to: 'scourses#index'
-  get '/tcourses', to: 'tcourses#index'
-  get '/global', to: 'global#index'
+    # routes to the mainpages
+    get '/homeworks', to: 'homeworks#index'
+    get '/classes', to: 'classes#index'
+    get '/exercises', to: 'exercises#index'
+    get '/scourses', to: 'scourses#index'
+    get '/tcourses', to: 'tcourses#index'
+    get '/global', to: 'global#index'
+
+
+    get '/', to: 'locals#change_locale', as: :change_locale
+  end
 
 end
