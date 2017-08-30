32abda1b7fe2ba9f74aa997e3dce75561446c674
Unknown <daniel.fasel@protonmail.com>
2017-08-30 19:19:40 +0300

    2

--- app/javascript/packs/general_helpers/menu_top/menu_top.vue
+++ app/javascript/packs/general_helpers/menu_top/menu_top.vue
@@ -1,67 +1,63 @@
 <template>
-  <div id="menu"  v-bind:class="{hide: menuHide}">
-    <slot></slot>
-  </div>
+<div id="menu" v-bind:class="{hide: menuHide}">
+  <slot></slot>
+</div>
 </template>
 
 <script>
 export default {
-data (){
-  return {
-    menuHide:false,
-    scrollHeight:0,
-    delayHeight:0
+  data() {
+    return {
+      menuHide: false,
+      scrollHeight: 0,
+      delayHeight: 0
 
-  }
-},
-methods: {
-     handleScroll: function (event) {
-        if (this.scrollHeight<=event.pageY && this.scrollHeight>100){
-            this.menuHide=true
-            this.delayHeight=event.pageY
-          }
+    }
+  },
+  methods: {
+    handleScroll: function(event) {
+      if (this.scrollHeight <= event.pageY && this.scrollHeight > 100) {
+        this.menuHide = true
+        this.delayHeight = event.pageY
+      } else if (this.scrollHeight > event.pageY) {
+        if (this.delayHeight == 0) {
+          this.delayHeight = event.pageY
+        }
+        var travDist = (this.delayHeight - event.pageY)
 
-          else if  (this.scrollHeight>event.pageY){
-            if (this.delayHeight==0) {
-                this.delayHeight=event.pageY
-            }
-              var travDist = (this.delayHeight-event.pageY)
-              console.log('delay' + this.delayHeight)
-              console.log('event' + event.pageY)
-              console.log(travDist)
-            if (travDist>30){
-              this.menuHide=false
+        if (travDist > 30) {
+          this.menuHide = false
 
-              this.delayHeight=0
+          this.delayHeight = 0
 
-            }
-          }
-
-        this.scrollHeight=event.pageY
+        }
       }
 
+      this.scrollHeight = event.pageY
+    }
 
- },
 
- created: function () {
-     window.addEventListener('scroll', this.handleScroll);
- },
- destroyed: function () {
-     window.removeEventListener('scroll', this.handleScroll);
- }
+  },
+
+  created: function() {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  destroyed: function() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
 }
 </script>
 
 <style scoped>
-#menu{
-  position:fixed;
-  top:0;
+#menu {
+  position: fixed;
+  top: 0;
   transition: margin-top 0.2s ease-in-out;
   z-index: 1;
   width: 100vw;
 }
 
-.hide{
+.hide {
   margin-top: -100px;
 }
 </style>
